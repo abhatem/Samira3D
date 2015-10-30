a2711c27e95e3e4f17f1b358610604720a5bf3f4
abo0ody26 <abo0ody2689@gmail.com>
2015-10-30 16:40:54 -0700

    Moved key_callback to an unnamed namespace

--- Samira3D/Input.cpp
+++ Samira3D/Input.cpp
@@ -7,33 +7,36 @@ namespace SE
 {
 	namespace Input
 	{
+		namespace
+		{
+			void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+			{
+				switch (action)
+				{
+				case GLFW_PRESS:
+					pressed(key);
+					break;
+				case GLFW_RELEASE:
+					released(key);
+					break;
+				case GLFW_REPEAT:
+					repeat(key);
+					break;
+				default:
+					break;
+				}
+			}
+		}
 		boost::signals2::signal<void(int)> pressed;
 		boost::signals2::signal<void(int)> released;
 		boost::signals2::signal<void(int)> repeat;
+
 
 		void init(GLFWwindow* window)
 		{
 			glfwSetKeyCallback(window, key_callback);
 		}
 
-
-		void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-		{
-			switch (action)
-			{
-			case GLFW_PRESS:
-				pressed(key);
-				break;
-			case GLFW_RELEASE:
-				released(key);
-				break;
-			case GLFW_REPEAT:
-				repeat(key);
-				break;
-			default:
-				break;
-			}
-		}
 		
 	}
 	
--- Samira3D/Input.h
+++ Samira3D/Input.h
@@ -9,7 +9,7 @@ namespace SE
 	namespace  Input
 	{
 		extern void init(GLFWwindow* window);
-		extern void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+		//extern void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 		extern boost::signals2::signal<void(int)> pressed;
 		extern boost::signals2::signal<void(int)> released;
 		extern boost::signals2::signal<void(int)> repeat;
